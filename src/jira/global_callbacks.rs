6acf7a94f74ec9d09a0a7d5c116ce4c3c7a20eee
Aleksandr <askiselev00@gmail.com>
2022-12-28 12:52:03 +0100

    Changed global callback

--- src/jira/global_callbacks.rs
+++ src/jira/global_callbacks.rs
@@ -1,10 +1,11 @@
 use cursive::Cursive;
-use super::constance::PROJECTS_SEARCH_VIEW_NAME;
+use super::constance::{PROJECTS_SEARCH_VIEW_NAME, TASKS_SEARCH_VIEW_NAME};
 
 pub(crate) fn add_global_callbacks(cursive: &mut Cursive) {
-    cursive.add_global_callback('p', go_to_project_search_callback)
-}
-
-fn go_to_project_search_callback(cursive: &mut Cursive) {
-    cursive.focus_name(PROJECTS_SEARCH_VIEW_NAME).unwrap();
+    cursive.add_global_callback('p', |cursive| {
+        cursive.focus_name(PROJECTS_SEARCH_VIEW_NAME).unwrap();
+    });
+    cursive.add_global_callback('t', |cursive| {
+        cursive.focus_name(TASKS_SEARCH_VIEW_NAME).unwrap();
+    });
 }
