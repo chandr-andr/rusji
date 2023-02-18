96531caf926f6e560d3d56858cf1b4802ac673e0
Aleksandr <askiselev00@gmail.com>
2023-02-19 01:29:40 +0400

    Small fixes

--- src/jira/constance.rs
+++ src/jira/constance.rs
@@ -1,6 +1,5 @@
 use cursive::align;
 
-pub(crate) const ACTIONS_SELECT_VIEW_NAME: &str = "actions_view";
 pub(crate) const INNER_CENTER_TOP_VIEW_ALIGN: align::Align = align::Align {
     h: align::HAlign::Center,
     v: align::VAlign::Top,
