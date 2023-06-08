8c3c7bdc8e5fcc22ba660ee7016ae774cbe83e7f
chandr-andr (Kiselev Aleksandr) <askiselev00@gmail.com>
2023-06-09 00:13:41 +0200

    Removed unnecessary view declaration.

--- src/jira/utils/views.rs
+++ src/jira/utils/views.rs
@@ -1,27 +1,9 @@
-use cursive::{
-    view::ViewWrapper,
-    views::{Dialog, TextView},
-    Cursive, View,
-};
+use cursive::{view::ViewWrapper, views::Dialog, Cursive, View};
 use rusji_derive::ViewWrapper;
 
 #[derive(ViewWrapper)]
 pub(crate) struct TryAgainView {
     inner_view: Dialog,
-}
-
-impl TryAgainView {
-    pub fn new<T>(error_text: &str, try_again_fn: T) -> Self
-    where
-        T: 'static + Fn(&mut Cursive),
-    {
-        Self {
-            inner_view: Dialog::new()
-                .title("Connection error!")
-                .content(TextView::new(error_text))
-                .button("Try again", try_again_fn),
-        }
-    }
 }
 
 #[derive(ViewWrapper)]
